5bc732fc097f69324f9bf86df51894d4a9a710d0
Reima Luoma <reima.luoma@gmail.com>
2023-05-08 12:45:36 +0300

    initial with dotnet webapi template

---
+++ helsinki-city-bike-api/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace helsinki_city_bike_api;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
