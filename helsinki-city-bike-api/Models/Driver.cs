fe03b0c36b0a0c42430324fb613d718e83dc35b9
Reima Luoma <reima.luoma@gmail.com>
2023-05-08 15:19:25 +0300

    Driver update
    Removed placeholders and added actual fields for data based on provided dataset.

--- helsinki-city-bike-api/Models/Driver.cs
+++ helsinki-city-bike-api/Models/Driver.cs
@@ -1,7 +1,12 @@
 namespace BikeApp.Drivers;
 
 public class Driver {
-    public int Id {get; set;}
-    public string Name {get; set;}
-    public int DriverNumber {get; set;}
+    public string Departure {get;set;}
+    public string Return {get;set;}
+    public string DepartureStationId {get;set;}
+    public string DepartureStationName {get;set;}
+    public string ReturnStationId {get;set;}
+    public string ReturnStationName {get;set;}
+    public int CoveredDistance {get;set;}
+    public int Duration {get;set;}
 }
